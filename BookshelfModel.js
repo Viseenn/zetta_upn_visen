acdfd64cd22836ef7039af06a380b714487c8328
Visen <visenthen@gmail.com>
2023-06-16 00:27:45 +0700

    MongoDB Day 5

---
+++ BookshelfModel.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const bookshelfSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  books: [{ type: mongoose.Schema.Types.ObjectId, ref: "Book" }],
+});
+
+const Bookshelf = mongoose.model("Bookshelf", bookshelfSchema);
+
+module.exports = Bookshelf;
